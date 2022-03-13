577a158174f0358d3908082f87e0effbf610f6a7
Catarina Montenegro <catarina.montenegro.silva@gmail.com>
2022-03-13 18:41:18 -0300

    add subtração function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ function calculateSoma() {
   const soma = 2 + 2
   document.getElementById("resultadoSoma").innerHTML = "A soma é " + soma;
 }
+
+function calculateSub() {
+  const subtrair = 2 - 2
+  document.getElementById("resultadoSub").innerHTML = "A subtração é" + subtrair;
+}
